9ab40811d530347aa24b563e7b154365bb24ceda
Krithik Rao <krdevmail@gmail.com>
2021-02-26 16:00:13 -0600

    Updated Rollup Output

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,22 @@
 import commonjs from '@rollup/plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
 
 export default {
   input: 'index.js',
-  output: {
-    file: 'bundle.js',
-    format: 'esm',
-    sourcemap: true
-  },
-  plugins: [commonjs()]
+  output: [
+    {
+      file: 'dist/bundle.js',
+      format: 'cjs'
+    },
+    {
+      file: 'dist/bundle.modern.js',
+      format: 'es'
+    },
+    {
+      file: 'dist/bundle.umd.js',
+      format: 'umd',
+      name: 'picoc'
+    }
+  ],
+  plugins: [nodeResolve(), commonjs()]
 };
